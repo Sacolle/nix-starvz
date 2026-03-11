ea855d871c42dced60da20eaf7b02dd6cd18e28c
sacolle <pedro.h.b.colle@gmail.com>
2026-03-11 09:15:37 -0300

    correção para poder executar nix run no starvzTools

--- starvzTools.nix
+++ starvzTools.nix
@@ -16,6 +16,13 @@
 stdenv.mkDerivation (f: 
 let
     myR = (rWrapper.override { packages = [ starvz ]; });
+    starvzPath = lib.makeBinPath [ 
+        myR 
+        poti 
+        pajeng 
+        StarPU 
+        recutils 
+    ];
 in
 {
     name = "starvz-tools";
@@ -43,22 +50,26 @@ in
     '';
 
     installPhase = ''
-      # tools/ (STARVZ_HOME/tools/)
-      mkdir -p $out/tools
-      cp inst/tools/*.sh inst/tools/*.R inst/tools/starvz inst/tools/starvz_csv.c $out/tools/
-      cp starvz_fast_csv_split $out/tools/
-      chmod +x $out/tools/*.sh $out/tools/starvz
-      patch
+        # tools/ (STARVZ_HOME/tools/)
+        mkdir -p $out/tools
+        cp inst/tools/*.sh inst/tools/*.R inst/tools/starvz inst/tools/starvz_csv.c $out/tools/
+        cp starvz_fast_csv_split $out/tools/
+        chmod +x $out/tools/*.sh $out/tools/starvz
+        patch
 
-      # etc/ (STARVZ_HOME/etc/)
-      mkdir -p $out/etc
-      cp -r inst/etc/* $out/etc/
+        # etc/ (STARVZ_HOME/etc/)
+        mkdir -p $out/etc
+        cp -r inst/etc/* $out/etc/
 
-      # bin/starvz — placed here so that STARVZ_HOME=$(dirname $0)/..=$out
-      mkdir -p $out/bin
-      cp $out/tools/starvz $out/bin/starvz
-      wrapProgram $out/bin/starvz \
-            --prefix PATH : ${lib.makeBinPath [ myR poti pajeng StarPU recutils]} \
+        # bin/starvz — placed here so that STARVZ_HOME=$(dirname $0)/..=$out
+        mkdir -p $out/bin
+        cp $out/tools/starvz $out/bin/starvz
+        
+        # add a symlink to the script, so when runing nix run it finds it
+        ln -sf $out/bin/starvz $out/bin/starvz-tools
+
+        wrapProgram $out/bin/starvz \
+            --prefix PATH : ${starvzPath} \
             --set R_LIBS_USER /nonexistent
     '';
 })
